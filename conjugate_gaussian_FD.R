47e34405183456ba1853d5a77ecb122f5f8fc0b0
Don-Li <yli877@aucklanduni.ac.nz>
2020-11-18 11:42:08 +1300

    conjugate gaussian add deriv_posterior function

--- conjugate_gaussian_FD.R
+++ conjugate_gaussian_FD.R
@@ -1,4 +1,26 @@
 
+# Derivatives for the target posterior
+
+deriv_posterior = function( N, xbar, sigma2,
+    a0, b0, mu0, lambda0, r_mu, r_tau){
+    # r_mu is a sample of mu from the CAVI approximation over which we compute the 
+        # Monte Carlo estimate of the gradient.
+    # Same for r_tau.
+    
+    deriv_p_mu_lik = r_tau * N * ( xbar - r_mu )
+    deriv_p_mu_prior = - lambda0 * r_tau * ( r_mu - mu0 )
+    deriv_p_tau_lik = N/2 * 1/r_tau - 1/2 * N * ( sigma2 + ( xbar - r_mu )^2 )
+    deriv_p_tau_prior = 1/r_tau * ( 1/2 + a0 - 1 ) -
+        1/2 * ( 
+            lambda0 * ( r_mu - mu0 )^2 + 2 * b0
+        )
+    
+    list(
+        d_mu_lik = deriv_p_mu_lik, d_mu_prior = deriv_p_mu_prior,
+        d_tau_lik = deriv_p_tau_lik, d_tau_prior = deriv_p_tau_prior
+    )
+}
+
 ##### Fisher divergence for CAVI conjugate Gaussian #####
 FD_CAVI_conjugate_guassian = function( x, a0, b0, mu0, lambda0, r_mu, r_tau, p_seq ){
     # r_mu is a sample of mu from the CAVI approximation over which we compute the 
@@ -10,13 +32,12 @@ FD_CAVI_conjugate_guassian = function( x, a0, b0, mu0, lambda0, r_mu, r_tau, p_s
     xbar = mean(x)
     sigma2 = sum( (x - xbar)^2 )/N
     
-    deriv_p_mu_lik = r_tau * N * ( xbar - r_mu )
-    deriv_p_mu_prior = - lambda0 * r_tau * ( r_mu - mu0 )
-    deriv_p_tau_lik = N/2 * 1/r_tau - 1/2 * N * ( sigma2 + ( xbar - r_mu )^2 )
-    deriv_p_tau_prior = 1/r_tau * ( 1/2 + a0 - 1 ) -
-        1/2 * ( 
-            lambda0 * ( r_mu - mu0 )^2 + 2 * b0
-        )
+    deriv_prior = deriv_posterior( N, xbar, sigma2,
+        a0, b0, mu0, lambda0, r_mu, r_tau )
+    deriv_p_mu_lik = deriv_prior$d_mu_lik
+    deriv_p_mu_prior = deriv_prior$d_mu_prior
+    deriv_p_tau_lik = deriv_prior$d_tau_lik
+    deriv_p_tau_prior = deriv_prior$d_tau_prior
     
     weight_mu = ( sum(x) + lambda0 * mu0)/(N + lambda0)
     
@@ -64,14 +85,12 @@ FD_ADVI_meanfield = function( x, a0, b0, mu0, lambda0,
     xbar = mean(x)
     sigma2 = sum( (x - xbar)^2 )/N
     
-    deriv_p_mu_lik = r_tau * N * ( xbar - r_mu )
-    deriv_p_mu_prior = - lambda0 * r_tau * ( r_mu - mu0 )
-    
-    deriv_p_tau_lik = N/2 * 1/r_tau - 1/2 * N * ( sigma2 + ( xbar - r_mu )^2 )
-    deriv_p_tau_prior = 1/r_tau * ( 1/2 + a0 - 1 ) -
-        1/2 * ( 
-            lambda0 * ( r_mu - mu0 )^2 + 2 * b0
-        )
+    deriv_prior = deriv_posterior( N, xbar, sigma2,
+        a0, b0, mu0, lambda0, r_mu, r_tau )
+    deriv_p_mu_lik = deriv_prior$d_mu_lik
+    deriv_p_mu_prior = deriv_prior$d_mu_prior
+    deriv_p_tau_lik = deriv_prior$d_tau_lik
+    deriv_p_tau_prior = deriv_prior$d_tau_prior
 
     powers = sapply( p_seq, function(p_){
         deriv_p_mu = p_ * deriv_p_mu_lik + deriv_p_mu_prior
@@ -113,14 +132,12 @@ FD_ADVI_fullrank = function( x, a0, b0, mu0, lambda0,
     sigma2 = sum( (x - xbar)^2 )/N
     
     # Derivatives for the target posterior
-    deriv_p_mu_lik = r_tau * N * ( xbar - r_mu )
-    deriv_p_mu_prior = - lambda0 * r_tau * ( r_mu - mu0 )
-    
-    deriv_p_tau_lik = N/2 * 1/r_tau - 1/2 * N * ( sigma2 + ( xbar - r_mu )^2 )
-    deriv_p_tau_prior = 1/r_tau * ( 1/2 + a0 - 1 ) -
-        1/2 * ( 
-            lambda0 * ( r_mu - mu0 )^2 + 2 * b0
-        )
+    deriv_prior = deriv_posterior( N, xbar, sigma2,
+        a0, b0, mu0, lambda0, r_mu, r_tau )
+    deriv_p_mu_lik = deriv_prior$d_mu_lik
+    deriv_p_mu_prior = deriv_prior$d_mu_prior
+    deriv_p_tau_lik = deriv_prior$d_tau_lik
+    deriv_p_tau_prior = deriv_prior$d_tau_prior
 
     powers = sapply( p_seq, function(p_){
         deriv_p_mu = p_ * deriv_p_mu_lik + deriv_p_mu_prior
@@ -136,27 +153,24 @@ FD_ADVI_fullrank = function( x, a0, b0, mu0, lambda0,
 }
 
 ##### Fisher divergence for Laplace approx of conjugate Gaussian {mu, tau} #####
-FD_Laplace_mu_tau = function( X, a0, b0, mu0, lambda0,
+FD_Laplace_mu_tau = function( x, a0, b0, mu0, lambda0,
     r_mu, r_tau, qmu_mu, qmu_var, qtau_mu, qtau_var,
     p_seq ){
-    N = length(X)
+    N = length(x)
     
     deriv_q_mu = d_dmu_log_norm( qmu_mu, qmu_var, r_mu )
     deriv_q_tau = d_dmu_log_norm( qtau_mu, qtau_var, r_tau )
     
-    x = X
     N = length(x)
     xbar = mean(x)
     sigma2 = sum( (x - xbar)^2 )/N
     
-    deriv_p_mu_lik = r_tau * N * ( xbar - r_mu )
-    deriv_p_mu_prior = - lambda0 * r_tau * ( r_mu - mu0 )
-    
-    deriv_p_tau_lik = N/2 * 1/r_tau - 1/2 * N * ( sigma2 + ( xbar - r_mu )^2 )
-    deriv_p_tau_prior = 1/r_tau * ( 1/2 + a0 - 1 ) -
-        1/2 * ( 
-            lambda0 * ( r_mu - mu0 )^2 + 2 * b0
-        )
+    deriv_prior = deriv_posterior( N, xbar, sigma2,
+        a0, b0, mu0, lambda0, r_mu, r_tau )
+    deriv_p_mu_lik = deriv_prior$d_mu_lik
+    deriv_p_mu_prior = deriv_prior$d_mu_prior
+    deriv_p_tau_lik = deriv_prior$d_tau_lik
+    deriv_p_tau_prior = deriv_prior$d_tau_prior
     
     powers = sapply( p_seq, function(p_){
         deriv_p_mu = p_ * deriv_p_mu_lik + deriv_p_mu_prior
@@ -171,29 +185,26 @@ FD_Laplace_mu_tau = function( X, a0, b0, mu0, lambda0,
 }
 
 ##### Fisher divergence for Laplace approx of conjugate Gaussian {mu, logtau} #####
-FD_Laplace_mu_logtau = function( X, a0, b0, mu0, lambda0, 
+FD_Laplace_mu_logtau = function( x, a0, b0, mu0, lambda0, 
     r_mu, r_logtau, qmu_mu, qmu_var, qlogtau_mu, qlogtau_var,
     p_seq ){
-    N = length(X)
+    N = length(x)
     
     r_tau = exp( r_logtau )
     
     deriv_q_mu = d_dmu_log_norm( qmu_mu, qmu_var, r_mu )
     deriv_q_tau = d_dmu_log_lnorm( qlogtau_mu, qlogtau_var, r_tau )
     
-    x = X
     N = length(x)
     xbar = mean(x)
     sigma2 = sum( (x - xbar)^2 )/N
     
-    deriv_p_mu_lik = r_tau * N * ( xbar - r_mu )
-    deriv_p_mu_prior = - lambda0 * r_tau * ( r_mu - mu0 )
-    
-    deriv_p_tau_lik = N/2 * 1/r_tau - 1/2 * N * ( sigma2 + ( xbar - r_mu )^2 )
-    deriv_p_tau_prior = 1/r_tau * ( 1/2 + a0 - 1 ) -
-        1/2 * ( 
-            lambda0 * ( r_mu - mu0 )^2 + 2 * b0
-        )
+    deriv_prior = deriv_posterior( N, xbar, sigma2,
+        a0, b0, mu0, lambda0, r_mu, r_tau )
+    deriv_p_mu_lik = deriv_prior$d_mu_lik
+    deriv_p_mu_prior = deriv_prior$d_mu_prior
+    deriv_p_tau_lik = deriv_prior$d_tau_lik
+    deriv_p_tau_prior = deriv_prior$d_tau_prior
     
     powers = sapply( p_seq, function(p_){
         deriv_p_mu = p_ * deriv_p_mu_lik + deriv_p_mu_prior
